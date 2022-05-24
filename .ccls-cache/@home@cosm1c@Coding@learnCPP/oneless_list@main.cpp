779c1c7e168a378ea4848b0c5109c991340dc1ce
1cosmic <notifylore@gmail.com>
2022-05-24 10:09:15 +0000

    Init oneless list.

--- .ccls-cache/@home@cosm1c@Coding@learnCPP/oneless_list@main.cpp
+++ .ccls-cache/@home@cosm1c@Coding@learnCPP/oneless_list@main.cpp
@@ -2,28 +2,25 @@
 
 template<typename T>
 class List {
-
     public:
         List();
         ~List();
     
     private:
         template<typename T1>
-        class Node {
-
-            public:
+            class Node {
                 T1 data;
                 Node *pNext;
 
                 Node(T1 data = T1(), Node *pNext = nullptr) {
                     this -> data = data;
                     this -> pNext = pNext;
+
                 }
 
-        };
+            };
 
         int counter;
-        Node<T> *head;
 };
 
 int main() {
